4048149ebc70628f33540a2981c45a22d0d05fda
olena <olena_iarova@epam.com>
2017-05-31 08:38:59 -0700

    Added hardcoded list of tasks, Added filtering by status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,53 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
-})
-export class AppComponent  { name = 'Angular'; }
+  export class TodoItem{
+    name: string;
+    status: string;
+  }
+
+
+  const TODOS: TodoItem[] = [
+  {name: 'Create a todo list', status: 'New'},
+  {name: 'Add multiple list items', status: 'New'},
+  {name: 'Change item status to comleted', status: 'Completed'},
+  {name: 'Delete item', status: 'New'}
+  ];
+
+
+  @Component({
+    selector: 'my-app',
+    template: `
+    <h1>Todo List</h1>
+  <div>
+    <input placeholder='What needs to be done'/>
+    </div>
+    <ul>
+    <li *ngFor="let todoItem of todos">
+    <p>{{todoItem.name}}</p>
+    <p>Status: {{todoItem.status}}</p>
+    </li>
+    </ul>
+   <button *ngFor="let status of statusesSet" (click)="filter(status)">{{status}}</button>
+    `,
+  })
+  export class AppComponent {
+    todos: Array;
+    statusesSet: Set<string> = new Set<string>();
+    constructor() {
+      this.todos = TODOS;
+      this.todos.forEach((item) => {
+        this.statusesSet.add(item.status);
+      })
+    }
+
+    filter(statusToFilter) {
+      this.todos = TODOS.filter((todo) => {
+        return todo.status === statusToFilter;
+      });
+  }
+
+
+
+
+
+  }
