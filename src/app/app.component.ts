a7062a947b9be98da4f0dad933cc39a7d6a82669
olena <olena_iarova@epam.com>
2017-06-01 09:48:24 -0700

    removed todos mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
   import { Component, OnInit } from '@angular/core';
-  import { TodosService} from './todos.service';
+  import { TodosService, TODOS} from './todos.service';
   import { Todo } from './todo'
-  import { TODOS } from './mock-todos';
 
   @Component({
     selector: 'my-app',
-    template: `
-    <h1>Todo List</h1>
-  <div>
-    <input placeholder='What needs to be done'/>
-    </div>
-    <ul>
-    <li *ngFor="let todoItem of todos">
-    <p>{{todoItem.name}}</p>
-    <p>Status: {{todoItem.status}}</p>
-    </li>
-    </ul>
-   <button *ngFor="let status of statusesSet" (click)="filter(status)">{{status}}</button>
-    `,
-   providers: [TodosService]
+    templateUrl: './app.component.html',
+    providers: [TodosService]
   })
 
   export class AppComponent implements OnInit{
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
-import { TODOS } from './mock-todos';
+
+export const TODOS: Todo[] = [
+{name: 'Create a todo list', status: 'New'},
+{name: 'Add multiple list items', status: 'New'},
+{name: 'Change item status to comleted', status: 'Completed'},
+{name: 'Delete item', status: 'New'}
+];
 
 
 @Injectable()
   export class TodosService {
     getTodos(): Promise<Todo[]>{
       return Promise.resolve(TODOS);
-    }
-  }
+    };
+
+}
