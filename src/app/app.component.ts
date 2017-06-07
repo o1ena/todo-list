e76d6bd39d2f8fc637c786e9b79e527ccc0f82af
olena <olena_iarova@epam.com>
2017-06-07 08:45:47 -0700

    removed hard coded list of todos. moved add/delete functions to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,50 +10,16 @@
 })
 
   export class AppComponent implements OnInit{
-    todos: Todo[];
     statusesSet: Set<string> = new Set<string>();
     constructor(private todosService: TodosService) {}
 
      getTodos(): void {
-        this.todosService.getTodos().then(todos => {
-          this.todos = todos;
-          this.todos.forEach((item) => {
+        this.todosService.todos.forEach((item) => {
           this.statusesSet.add(item.status);
         })
-      });
      }
 
       ngOnInit(): void {
         this.getTodos();
       }
-
-
-      addTodoItem(newTodo: string){
-        this.todos.push(newTodo);
-        console.log('added');
-    }
-
-    deleteTodoItem(name: string) : void {
-    var index = this.todos.indexOf(name, 0);
-    if (index !== undefined) {
-     this.todos.splice(index, 1);
-  }
 }
-
-  deleteSelectedTodos() {
-        for(var i=(this.todos.length -1); i > -1; i--) {
-        if(this.todos[i].completed) {
-          this.todos.splice(i, 1);
-       }
-      }
-    }
-
-
-       filter(statusToFilter:any) {
-       this.todos = TODOS.filter((todo) => {
-        return todo.status === statusToFilter;
-      });
-  }
-
-
-  }
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -2,17 +2,37 @@ import { Injectable } from '@angular/core';
 import { Todo } from './todo';
 
 
-export const TODOS: Todo[] = [
-{name: 'Create a todo list', status: 'New', completed: false},
-{name: 'Add multiple list items', status: 'New', completed: false},
-{name: 'Change item status to comleted', status: 'Completed', completed: true},
-{name: 'Delete item', status: 'New',completed: false}
-];
-
 @Injectable()
   export class TodosService {
-    getTodos(): Promise<Todo[]>{
-      return Promise.resolve(TODOS);
+    todos: Array<Todo> = [];
+    addTodoItem(newTodo: string){
+        this.todos.push(newTodo);
+        console.log('added');
+    }
+
+    deleteTodoItem(name: string) : void {
+        var index = this.todos.indexOf(name, 0);
+        if (index !== undefined) {
+            this.todos.splice(index, 1);
+        }
+    }
+
+    deleteSelectedTodos() {
+      for(var i=(this.todos.length -1); i > -1; i--) {
+        if(this.todos[i].completed) {
+          this.todos.splice(i, 1);
+        }
+      }
+  }
+
+    getTodos(): Array<Todo> {
+      return this.todos;
+    }
+
+    filter(statusToFilter:any) {
+        this.todos = TODOS.filter((todo) => {
+          return todo.status === statusToFilter;
+        });
     }
 }
 
